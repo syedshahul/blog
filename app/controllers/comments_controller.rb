8b7f059a8c9d264d8e0079081d6914d4f054dd50
syedshahul <syedshahul2002@gmail.com>
2014-05-15 17:35:34 +0530

    completing 8

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,13 @@ class CommentsController < ApplicationController
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
-
+# delete comment by id
+  def destroy
+    @article =Article.find(params[:article_id])
+    @comment =@article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body)
