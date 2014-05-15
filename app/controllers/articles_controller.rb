0cea2e6867bcc77dc36dcff35a937e563b795842
syedshahul <syedshahul2002@gmail.com>
2014-05-15 16:56:52 +0530

    5.9 to 5.12

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,21 +5,49 @@ class ArticlesController < ApplicationController
   end
   #new method
   def new
+    @article = Article.new
   end
   #create article method
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+       redirect_to @article
+    else
+       render 'new'
+    end
   end
   # show article by id
   def show
     @article = Article.find(params[:id])
   end
+  #edit article by id
+  def edit
+    @article = Article.find(params[:id])
+  end
+  #update
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  #delete by id
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+  #private methods
   private
   def article_params
+    puts "article #{params[:article]} #{params[:id]} #{params[:title]}
+#{params[:text]}"
     params.require(:article).permit(:title, :text)
   end
 end
